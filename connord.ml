c4a61021eb20d71207a28a3a2f60d34006b7134d
Connor-Damato <damatoconnor@gmail.com>
2024-09-18 12:02:43 -0400

    test: commented out tests

--- connord.ml
+++ connord.ml
@@ -8,7 +8,7 @@ let rec binomial n k =
   if n = k || k = 0 then 1
   else binomial (n - 1) (k - 1) + binomial (n - 1) k;;
 
-binomial 5 3;;
+(* binomial 5 3;; *)
 
 (* Question 8 *)
 let rec dup input = 
@@ -16,7 +16,7 @@ let rec dup input =
   | 0 -> []
   | n -> [List.hd input;List.hd input]@dup (List.tl input);;
 
-dup [1;2;3;4];;
+(* dup [1;2;3;4];; *)
 
 (* Question 9 *)
 let rec undup input = 
@@ -25,8 +25,8 @@ let rec undup input =
   | x :: [] -> raise (Failure "uneven length list")
   | x :: y :: rest -> if x != y then raise (Failure "non-symmetrical list") else x::undup rest;;
 
-undup [1;1;2;2;3;3;4;4];;
+(* undup [1;1;2;2;3;3;4;4];;
 undup (dup [1;2;3;4]);;
 undup [];;
 undup [1;1;2;2;3;0;4;4];;
-undup [1;1;2;2;3;3;4];;
+undup [1;1;2;2;3;3;4];; *)
