e82966fd1358ca78639d9adebd27bfebf17bd1d5
Connor-Damato <damatoconnor@gmail.com>
2024-09-18 13:52:02 -0400

    fix: removed if statements

--- connord.ml
+++ connord.ml
@@ -1,16 +1,21 @@
-(* Question 6 *)
+(* Problem 6 Solution *)
+(* Author: Connor Damato *)
 let makepairs x input = List.map (fun y -> (x,y)) input;;
 
 (* makepairs 5 [1;2;3;4];; *)
 
-(* Question 7 *)
+(* Problem 7 Solution *)
+(* Author: Connor Damato *)
 let rec binomial n k =
-  if n = k || k = 0 then 1
-  else binomial (n - 1) (k - 1) + binomial (n - 1) k;;
+  match k with
+  | 0 -> 1
+  | x when n = k -> 1
+  | x -> binomial (n - 1) (k - 1) + binomial (n - 1) k;;
 
-(* binomial 5 3;; *)
+binomial 5 3;;
 
-(* Question 8 *)
+(* Problem 8 Solution *)
+(* Author: Connor Damato *)
 let rec dup input = 
   match List.length input with
   | 0 -> []
@@ -18,12 +23,13 @@ let rec dup input =
 
 (* dup [1;2;3;4];; *)
 
-(* Question 9 *)
+(* Problem 9 Solution *)
+(* Author: Connor Damato *)
 let rec undup input = 
   match input with
   | [] -> []
   | x :: [] -> raise (Failure "uneven length list")
-  | x :: y :: rest -> if x != y then raise (Failure "non-symmetrical list") else x::undup rest;;
+  | x :: y :: rest -> (match x != y with | true -> raise (Failure "non-symmetrical list") | false -> x::undup rest);;
 
 (* undup [1;1;2;2;3;3;4;4];;
 undup (dup [1;2;3;4]);;
